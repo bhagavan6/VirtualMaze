309208c00c93d70f02765b989e3422417613dcc1
Bhagavan <nagalingabhagavan@gmail.com>
2026-09-11 22:54:46 +0530

    Largest and Smallest in the Array

---
+++ Day1/arr.js
@@ -0,0 +1,13 @@
+let arr=[25,73,52,82,17];
+let largest=arr[0];
+let smallest=arr[0];
+for(let i=1;i<arr.length;i++){
+    if(arr[i]>largest){
+        largest=arr[i];
+    }
+    if(arr[i]<smallest){
+        smallest=arr[i];
+    }
+}
+console.log(`Largest: ${largest}`);
+console.log(`Smallest: ${smallest}`);
